da23629db541e97ed55c740b59bf52419f325183
Sebastian Pipping <sebastian@pipping.org>
2021-08-15 21:31:36 +0200

    gentoo-tree-diff: tests: Cover function "iterate_new_and_changed_ebuilds"

--- binary_gentoo/internal/cli/tests/test_tree_diff.py
+++ binary_gentoo/internal/cli/tests/test_tree_diff.py
@@ -2,6 +2,7 @@
 # Licensed under GNU Affero GPL version 3 or later
 
 import os
+from contextlib import contextmanager
 from io import StringIO
 from tempfile import TemporaryDirectory
 from textwrap import dedent
@@ -10,8 +11,8 @@ from unittest.mock import patch
 
 from parameterized import parameterized
 
-from ..tree_diff import (_replace_special_keywords_for_ebuild, enrich_config, main,
-                         parse_command_line)
+from ..tree_diff import (_replace_special_keywords_for_ebuild, enrich_config,
+                         iterate_new_and_changed_ebuilds, main, parse_command_line)
 
 
 class ReplaceSpecialKeywordsTest(TestCase):
@@ -58,6 +59,120 @@ class EnrichConfigTest(TestCase):
         with patch('subprocess.check_output', self._fake_subprocess_check_output):
             enrich_config(config)
         self.assertEqual(config.keywords, {'one', 'two', '~*'})
+
+
+class IterateNewAndChangedEbuildsTest(TestCase):
+    @classmethod
+    @contextmanager
+    def _tempdir_config(
+        cls,
+        keywords: str,
+        pessimistic: bool = False,
+    ):
+        with TemporaryDirectory() as temp_old_portdir, \
+                TemporaryDirectory() as temp_new_portdir:
+            argv = ['gentoo-tree-diff', '--keywords', keywords]
+            if pessimistic:
+                argv.append('--pessimistic')
+            argv += [temp_old_portdir, temp_new_portdir]
+
+            config = parse_command_line(argv)
+            enrich_config(config)
+
+            yield config
+
+    @classmethod
+    def _create_ebuild(cls,
+                       portdir,
+                       ebuild_filename,
+                       keywords: str = None,
+                       extra_content: str = None):
+        filename = os.path.join(portdir, ebuild_filename)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'w') as f:
+            if keywords is not None:
+                print(f'KEYWORDS="{keywords}"', file=f)
+            if extra_content is not None:
+                print(extra_content, file=f)
+            f.flush()
+        with open(filename) as f:
+            print('CONTENT', f.read())
+
+    def test_new_live_ebuild_ignored_by_filename(self):
+        keywords = 'one'
+        with self._tempdir_config(keywords=keywords) as config:
+            self._create_ebuild(config.new_portdir, 'cat/pkg/pkg-123.ebuild', keywords=keywords)
+            self._create_ebuild(config.new_portdir, 'cat/pkg/pkg-9999.ebuild', keywords=keywords)
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        self.assertEqual(actual_news, ['cat/pkg-123'])
+
+    def test_new_ebuild_without_keyword_line_ignored(self):
+        keywords = 'one'
+        with self._tempdir_config(keywords='one') as config:
+            self._create_ebuild(config.new_portdir, 'cat/pkg/pkg-123.ebuild', keywords=keywords)
+            self._create_ebuild(config.new_portdir, 'cat/pkg/pkg-456.ebuild', keywords=None)
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        self.assertEqual(actual_news, ['cat/pkg-123'])
+
+    def test_new_ebuild_without_matching_keyword_ignored(self):
+        keywords = 'one'
+        with self._tempdir_config(keywords='one') as config:
+            self._create_ebuild(config.new_portdir, 'cat/pkg/pkg-123.ebuild', keywords=keywords)
+            self._create_ebuild(config.new_portdir, 'cat/pkg/pkg-456.ebuild', keywords='other')
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        self.assertEqual(actual_news, ['cat/pkg-123'])
+
+    def test_unchanged_file_ignored(self):
+        keywords = 'one'
+        ebuild_filename = 'cat/pkg/pkg-123.ebuild'
+        with self._tempdir_config(keywords=keywords) as config:
+            self._create_ebuild(config.old_portdir, ebuild_filename, keywords=keywords)
+            self._create_ebuild(config.new_portdir, ebuild_filename, keywords=keywords)
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        self.assertEqual(actual_news, [])
+
+    def test_changed_ebuild_without_matching_keywords_ignored(self):
+        keywords = 'one'
+        ebuild_filename = 'cat/pkg/pkg-123.ebuild'
+        with self._tempdir_config(keywords='one') as config:
+            self._create_ebuild(config.old_portdir, ebuild_filename, keywords=keywords)
+            self._create_ebuild(config.new_portdir, ebuild_filename, keywords='other')
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        self.assertEqual(actual_news, [])
+
+    @parameterized.expand([
+        ('pessimistic, not ignored', True),
+        ('not pessimistic, ignored', False),
+    ])
+    def test_changed_ebuild_with_matching_identical_keywords(self, _, pessimistic: bool):
+        keywords = 'one'
+        ebuild_filename = 'cat/pkg/pkg-123.ebuild'
+        with self._tempdir_config(keywords=keywords, pessimistic=pessimistic) as config:
+            self._create_ebuild(config.old_portdir,
+                                ebuild_filename,
+                                keywords=keywords,
+                                extra_content='# old')
+            self._create_ebuild(config.new_portdir,
+                                ebuild_filename,
+                                keywords=keywords,
+                                extra_content='# new')
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        expected_news = ['cat/pkg-123'] if pessimistic else []
+        self.assertEqual(actual_news, expected_news)
+
+    @parameterized.expand([
+        ('pessimistic, not ignored', True),
+        ('not pessimistic, not ignored', False),
+    ])
+    def test_changed_ebuild_with_matching_changed_keywords(self, _, pessimistic: bool):
+        ebuild_filename = 'cat/pkg/pkg-123.ebuild'
+        with self._tempdir_config(keywords='one', pessimistic=pessimistic) as config:
+            self._create_ebuild(config.old_portdir, ebuild_filename,
+                                keywords='~one')  # did not match keywords, previously
+            self._create_ebuild(config.new_portdir, ebuild_filename,
+                                keywords='one')  # just went stable, now matches keywords
+            actual_news = list(iterate_new_and_changed_ebuilds(config))
+        self.assertEqual(actual_news, ['cat/pkg-123'])
 
 
 class MainTest(TestCase):
